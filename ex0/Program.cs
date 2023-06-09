d3d97234c5cb7a201672c2c9c05a8bb17b388f45
Anzhelika <anjelichka2112@mail.ru>
2023-06-09 22:13:59 +0300

    добавили папку ex0

---
+++ ex0/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Console.Clear();
+Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+int result = userNumber*userNumber;
+
+Console.WriteLine($"{userNumber} -> {result}");
+*/
+
+
+
+
+/*
+Console.Clear();
+Console.Write("Введите первое целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+Console.Write("Введите второе целое число: ");
+string userInput1= Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+if(userNumber == userNumber1*userNumber1){
+    Console.WriteLine("Yes");} 
+if(userNumber != userNumber1*userNumber1){
+    Console.WriteLine("No");}
+*/
+
+
+
+
+
+/*
+Console.Clear();
+Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+if(userNumber == 1){
+    Console.WriteLine("Понедельник");} 
+if(userNumber == 2){
+    Console.WriteLine("Вторник");} 
+if(userNumber == 3){
+    Console.WriteLine("Среда");} 
+if(userNumber == 4){
+    Console.WriteLine("Четверг");} 
+if(userNumber == 5){
+    Console.WriteLine("Пятница");} 
+if(userNumber == 6){
+    Console.WriteLine("Суббота");} 
+if(userNumber == 7){
+    Console.WriteLine("Воскресенье");} 
+*/
